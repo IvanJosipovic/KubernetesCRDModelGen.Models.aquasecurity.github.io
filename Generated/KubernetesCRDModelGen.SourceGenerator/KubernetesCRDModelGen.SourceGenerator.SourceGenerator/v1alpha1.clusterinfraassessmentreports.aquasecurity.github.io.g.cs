b2eb7b7bbd5fd5a954ece7ddcb7f420c23235ea8
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-04-27 18:53:54 -0700

    feat: Update CRDs (#8)

--- Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.clusterinfraassessmentreports.aquasecurity.github.io.g.cs
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.clusterinfraassessmentreports.aquasecurity.github.io.g.cs
@@ -10,7 +10,7 @@ using System.Text.Json.Serialization;
 
 namespace KubernetesCRDModelGen.Models.aquasecurity.github.io;
 /// <summary>ClusterInfraAssessmentReport is a specification for the ClusterInfraAssessmentReport resource.</summary>
-[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.3+0316f97acf12de380316633df08e7f13a064978d")]
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
 public partial class V1alpha1ClusterInfraAssessmentReportList : IKubernetesObject<V1ListMeta>, IItems<V1alpha1ClusterInfraAssessmentReport>
@@ -37,7 +37,7 @@ public partial class V1alpha1ClusterInfraAssessmentReportList : IKubernetesObjec
 }
 
 /// <summary>Scope indicates the section of config that was audited.</summary>
-[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.3+0316f97acf12de380316633df08e7f13a064978d")]
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1ClusterInfraAssessmentReportReportChecksScope
 {
@@ -51,7 +51,7 @@ public partial class V1alpha1ClusterInfraAssessmentReportReportChecksScope
 }
 
 /// <summary>Check provides the result of conducting a single audit step.</summary>
-[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.3+0316f97acf12de380316633df08e7f13a064978d")]
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1ClusterInfraAssessmentReportReportChecks
 {
@@ -87,7 +87,7 @@ public partial class V1alpha1ClusterInfraAssessmentReportReportChecks
 }
 
 /// <summary>Scanner is the spec for a scanner generating a security assessment report.</summary>
-[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.3+0316f97acf12de380316633df08e7f13a064978d")]
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1ClusterInfraAssessmentReportReportScanner
 {
@@ -105,7 +105,7 @@ public partial class V1alpha1ClusterInfraAssessmentReportReportScanner
 }
 
 /// <summary>InfraAssessmentSummary counts failed checks by severity.</summary>
-[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.3+0316f97acf12de380316633df08e7f13a064978d")]
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1ClusterInfraAssessmentReportReportSummary
 {
@@ -126,7 +126,7 @@ public partial class V1alpha1ClusterInfraAssessmentReportReportSummary
     public required int MediumCount { get; set; }
 }
 
-[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.3+0316f97acf12de380316633df08e7f13a064978d")]
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1ClusterInfraAssessmentReportReport
 {
@@ -144,7 +144,7 @@ public partial class V1alpha1ClusterInfraAssessmentReportReport
 }
 
 /// <summary>ClusterInfraAssessmentReport is a specification for the ClusterInfraAssessmentReport resource.</summary>
-[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.3+0316f97acf12de380316633df08e7f13a064978d")]
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
 public partial class V1alpha1ClusterInfraAssessmentReport : IKubernetesObject<V1ObjectMeta>
